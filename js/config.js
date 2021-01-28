7280e556216d9b21789d24176c76aae4ab9c1de8
Masayuki Matsuo <masa.y.m@gmail.com>
2021-01-28 09:30:37 +0900

    set config of user pool id and user client id

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_bYFfaKMSw',
+        userPoolClientId: '2esd9q4tp0dvl1t2rlii0ia8ld',
+        region: 'ap-northeast-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
